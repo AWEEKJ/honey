67a706401652944bb885af892af121bd6f90f4b1
Hanju Jo <gkswn121@gmail.com>
2016-10-06 20:05:28 +0900

    Add slack token

--- settings.py
+++ settings.py
@@ -4,6 +4,6 @@ from __future__ import unicode_literals
 
 APPS = ['hello_world', 'helper', 'ghost']
 
-SLACK_TOKEN = None
+SLACK_TOKEN = 'xoxb-88117863477-fIvQYZBDfbQJr62mwNiIl3pH'
 
 REDIS_URL = None
